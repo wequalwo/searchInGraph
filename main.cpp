40e6a8be2c0d5bea2a9a46e3e99ef0a99ec481f6
cher <romacherepanov2002@gmail.com>
2025-03-14 14:28:20 +0300

    main usage update

--- main.cpp
+++ main.cpp
@@ -7,14 +7,18 @@ int main(int argc, char *argv[])
 {
 	if (argc != 5)
     {
-        std::cerr << "Usage: " << argv[0] << " <n>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <n> <d> <g> <s>" << std::endl;
+        std::cerr << "<n> = vertices number for experiment\n";
+        std::cerr << "<d> = density for experiment\n";
+        std::cerr << "<g> = number of graphs to generate for experiment\n";
+        std::cerr << "<s> = number of searches run on each graph\n";
         return 1;
     }
 
     int n = std::atoi(argv[1]);          // Получаем количество вершин из аргументов командной строки
     double density = std::atof(argv[2]); // Получаем значение плотности из аргументов командной строки
-    int graphs = std::atoi(argv[3]);     // Количество графов
-    int searches = std::atoi(argv[4]);   // Количество запусков
+    int graphs = std::atoi(argv[3]);     // Количество графов для каждой плотности
+    int searches = std::atoi(argv[4]);   // Количество запусков для каждого графа
 
 	Logger log("logger/log.txt", "logger/err.txt");
     MonteCarlo mc({density}, n, graphs, searches, log);
