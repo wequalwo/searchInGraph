2753da42f5f2bc44ab96ca2e8eae9778eccb9124
cher <romacherepanov2002@gmail.com>
2025-03-15 17:12:51 +0300

    any number of densities in cmd

--- main.cpp
+++ main.cpp
@@ -5,26 +5,29 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 5)
+	if (argc < 5)
     {
-        std::cerr << "Usage: " << argv[0] << " <n> <d> <g> <s>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <n> <g> <s> <d0> <d1> ... <dn>" << std::endl;
         std::cerr << "<n> = vertices number for experiment\n";
-        std::cerr << "<d> = density for experiment\n";
         std::cerr << "<g> = number of graphs to generate for experiment\n";
         std::cerr << "<s> = number of searches run on each graph\n";
+        std::cerr << "<di> = densities for experiment\n";
         return 1;
     }
 
+
     int n = std::atoi(argv[1]);          // Получаем количество вершин из аргументов командной строки
-    double density = std::atof(argv[2]); // Получаем значение плотности из аргументов командной строки
-    int graphs = std::atoi(argv[3]);     // Количество графов для каждой плотности
-    int searches = std::atoi(argv[4]);   // Количество запусков для каждого графа
+    int graphs = std::atoi(argv[2]);     // Количество графов для каждой плотности
+    int searches = std::atoi(argv[3]);   // Количество запусков для каждого графа
+
+    List<double> densities;
+    for (unsigned i = 4; i < argc; ++i)
+        densities.push_back(std::atof(argv[i])); // Получаем значение плотности из аргументов командной строки
 
 	Logger log("logger/log.txt", "logger/err.txt");
-    MonteCarlo mc({density}, n, graphs, searches, log);
+    MonteCarlo mc(densities, n, graphs, searches, log);
 
     mc.initialize();
-
 
 	return 0;
 }
