78fab22cd35f40989f036e58fe03f01a4d84ce42
wequalwo <s.k.ivanov315@gmail.com>
2025-05-11 18:40:55 +0300

    logger.h is back

---
+++ logger/logger.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <fstream>
+#include <string>
+
+#include "common/common.h"
+#include "graph/node.h"
+
+class Logger
+{
+public:
+    Logger(const std::string& log, const std::string& err, SizeType threadIdx);
+
+    Logger(const Logger& other) = delete;
+    Logger(Logger&& other) = default;
+    ~Logger();
+
+    void errSearch(const std::string& errTxt, SizeType graphSize, double density, SizeType from, SizeType to,
+                   const std::string& searchType);
+    void logErrGraph(const List<Node>& graph);
+    void errBuild(const std::string& errTxt, SizeType graphSize, double density);
+    void log(SizeType graphSize, double density, SizeType dist, SizeType bfs, SizeType dfs);
+private:
+    std::ofstream m_log;
+    std::ofstream m_err;
+};
+
+
+#endif // LOGGER_H
