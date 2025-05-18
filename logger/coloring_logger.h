2b93a9ee1000bbc62c0007fb106704a6bbcfeaf8
cher <romacherepanov2002@gmail.com>
2025-05-18 21:15:01 +0300

    rand coloring

---
+++ logger/coloring_logger.h
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef COLORING_LOGGER_H
+#define COLORING_LOGGER_H
+
+#include "logger/logger.h"
+
+class ColoringLogger : public Logger
+{
+public:
+    ColoringLogger(const std::string& log, const std::string& err, SizeType threadIdx)
+        : Logger(log, err, threadIdx)
+    {};
+
+    ColoringLogger(const ColoringLogger& other) = delete;
+    ColoringLogger(ColoringLogger&& other) = default;
+
+    void errColoring(const std::string& errTxt, SizeType graphSize, double density, const std::string& coloring);
+    void log(SizeType graphSize, double density, SizeType naive, SizeType greedy, const List<uint32_t>& tries);
+};
+
+#endif // COLORING_LOGGER_H
