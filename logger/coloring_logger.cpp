7971ac56e7bb523fbc94027a59af594f787f1c73
cher <romacherepanov2002@gmail.com>
2025-05-18 23:25:59 +0300

    space fix

--- logger/coloring_logger.cpp
+++ logger/coloring_logger.cpp
@@ -12,7 +12,7 @@ void ColoringLogger::errColoring(const std::string& errTxt, SizeType graphSize, 
 void ColoringLogger::log(SizeType graphSize, double density, SizeType naive, SizeType greedy, const List<uint32_t>& tries)
 {
     // пока лог упоротый, но зато отдельные части независимы
-    m_log << graphSize << ' ' << density << ' ' << naive << ' ' << greedy << ' '
+    m_log << graphSize << ' ' << density << ' ' << naive << ' ' << greedy
           << std::accumulate(tries.begin(), tries.end(), std::string(),
                              [] (const std::string& res, uint32_t cur)
                              { return res + ' ' + std::to_string(cur); }) << std::endl;
